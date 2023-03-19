dec6c67e91c91e742f8fb109c88bac9c9dda20bb
hip06 <khongpikten21@gmail.com>
2023-03-19 19:38:40 +0700

    ok

--- scrapeController.js
+++ scrapeController.js
@@ -2,39 +2,26 @@ const scrapers = require('./scraper')
 const fs = require('fs')
 
 const scrapeController = async (browserInstance) => {
-    const url = 'https://phongtro123.com/'
-    const indexs = [1, 2, 3, 4]
+    const url = 'https://digital-world-2.myshopify.com/'
     try {
         let browser = await browserInstance
         // gọi hàm cạo ở file s scrape
         const categories = await scrapers.scrapeCategory(browser, url)
-        const selectedCategories = categories.filter((category, index) => indexs.some(i => i === index))
-
-        // let result1 = await scrapers.scraper(browser, selectedCategories[0].link)
-        // fs.writeFile('chothuephongtro.json', JSON.stringify(result1), (err) => {
-        //     if (err) console.log('Ghi data vô file json thất bại: ' + err)
-        //     console.log('Thêm data thanh công !.')
-        // })
-
-        let result2 = await scrapers.scraper(browser, selectedCategories[1].link)
-        fs.writeFile('nhachothue.json', JSON.stringify(result2), (err) => {
-            if (err) console.log('Ghi data vô file json thất bại: ' + err)
-            console.log('Thêm data thanh công !.')
-        })
-        let result3 = await scrapers.scraper(browser, selectedCategories[2].link)
-        fs.writeFile('chothuecanho.json', JSON.stringify(result3), (err) => {
-            if (err) console.log('Ghi data vô file json thất bại: ' + err)
-            console.log('Thêm data thanh công !.')
-        })
-        let result4 = await scrapers.scraper(browser, selectedCategories[3].link)
-        fs.writeFile('chothuematbang.json', JSON.stringify(result4), (err) => {
-            if (err) console.log('Ghi data vô file json thất bại: ' + err)
-            console.log('Thêm data thanh công !.')
+        // console.log(categories);
+        categories.forEach(async (category) => {
+            const items = await scrapers.scrapeItems(browser, category.link)
+            console.log(items);
+            // items.forEach(async (item) => {
+            //     const rs = await scrapers.scraper(browser, item)
+            //     fs.writeFile('ecommerce.json', JSON.stringify({ [category]: rs }), (err) => {
+            //         if (err) console.log('Ghi data vô file json thất bại: ' + err)
+            //         console.log('Thêm data thanh công !.')
+            //     })
+            // })
         })
         await browser.close()
     } catch (e) {
         console.log('Lỗi ở scrape controller: ' + e);
-
     }
 }
 
--- scraper.js
+++ scraper.js
@@ -4,13 +4,13 @@ const scrapeCategory = (browser, url) => new Promise(async (resolve, reject) => 
         console.log('>> Mở tab mới ...');
         await page.goto(url)
         console.log('>>Truy cập vào ' + url)
-        await page.waitForSelector('#webpage')
+        await page.waitForSelector('#shopify-section-all-collections')
         console.log('>> Website đã laod xong...');
 
-        const dataCategory = await page.$$eval('#navbar-menu > ul > li', els => {
+        const dataCategory = await page.$$eval('#shopify-section-all-collections > div.all-collections > div.sdcollections-content > ul.sdcollections-list > li', els => {
             dataCategory = els.map(el => {
                 return {
-                    category: el.querySelector('a').innerText,
+                    category: el.querySelector('div.collection-name').innerText,
                     link: el.querySelector('a').href
                 }
             })
@@ -25,6 +25,30 @@ const scrapeCategory = (browser, url) => new Promise(async (resolve, reject) => 
         reject(error)
     }
 })
+const scrapeItems = (browser, url) => new Promise(async (resolve, reject) => {
+    try {
+        let page = await browser.newPage()
+        console.log('>> Mở tab mới ...');
+        await page.goto(url)
+        console.log('>>Truy cập vào ' + url)
+        await page.waitForSelector('#collection_content')
+        console.log('>> Website đã laod xong...');
+
+        const items = await page.$$eval('#collection-product-grid > div.grid-element', els => {
+            items = els.map(el => {
+                return el.querySelector('a.grid-view-item__link').href
+            })
+            return items
+        })
+        // await page.close()
+        // console.log('>> Tab đã đóng.');
+        resolve(items)
+
+    } catch (error) {
+        console.log('lỗi ở scrape items: ' + error)
+        reject(error)
+    }
+})
 
 const scraper = (browser, url) => new Promise(async (resolve, reject) => {
     try {
@@ -32,117 +56,178 @@ const scraper = (browser, url) => new Promise(async (resolve, reject) => {
         console.log('>> Đã mở tab mới ...');
         await newPage.goto(url)
         console.log(">> Đã truy cập vào trang " + url)
-        await newPage.waitForSelector('#main')
+        await newPage.waitForSelector('#shopify-section-product-template')
         console.log('>> Đã load xong tag main ...');
 
         const scrapeData = {}
 
-        // lấy header
-        const headerData = await newPage.$eval('header', (el) => {
-            return {
-                title: el.querySelector('h1').innerText,
-                description: el.querySelector('p').innerText
-            }
+        // lấy tên sản phẩm
+        const name = await newPage.$eval('header.section-header', (el) => {
+            return el.querySelector('h3')?.innerText
         })
-        scrapeData.header = headerData
+        scrapeData.name = name
 
-        // lấy links detail item
-        const detailLinks = await newPage.$$eval('#left-col > section.section-post-listing > ul > li', (els) => {
-            detailLinks = els.map(el => {
-                return el.querySelector('.post-meta > h3 > a').href
-            })
-            return detailLinks
+        // Lấy ảnh sp
+        const thumb = await newPage.$eval('#ProductPhoto', (el) => {
+            return el.querySelector('#ProductPhotoImg')?.src
         })
+        scrapeData.thumb = thumb
+        const images = await newPage.$$eval('#ProductThumbs > div.owl-wrapper-outer > div.owl-wrapper > div.owl-item', (els) => {
+            images = els.map(el => {
+                return el.querySelector('a.product-single__thumbnail').href
+            })
+            return images
+        })
+        scrapeData.images = images
+
+
+        // Lấy giá sp
+        const price = await newPage.$eval('#ProductPrice', (el) => {
+            return el.querySelector('span.money')?.innerText
+        })
+        scrapeData.price = price
+
+
+        // Lấy mô tả sp
+        const description = await newPage.$$eval('div.product-single__description > ul > li', (els) => {
+            description = els.map(el => {
+                return el?.innerText
+            })
+            return description
+        })
+        scrapeData.description = description
+
+        // lấy variants
+        const variants = await newPage.$$eval('form.product-single__form > div.product-form__item', (els) => {
+            variants = els.map(el => {
+                const variants = el.querySelectorAll('fieldset.single-option-radio > label')
+                const values = []
+                for (let variant of variants) values.push(variant?.innerText)
+                return {
+                    label: el.querySelector('label.single-option-radio__label')?.innerText,
+                    variants: values
+                }
+            })
+            return variants
+        })
+        scrapeData.variants = variants
+
+        // Lấy thông tin sp
+        const infomationTitles = await newPage.$$eval('#tabs-information > ul > li', (els) => {
+            infomationTitles = els.map(el => {
+                return el.querySelector('a')?.innerText
+            })
+            return infomationTitles
+        })
+        const desc = await newPage.$eval('#desc', (el) => {
+            return el?.textContent
+        })
+        const size = await newPage.$eval('#size', (el) => {
+            return el?.innerText
+        })
+        const delivery = await newPage.$eval('#delivery', (el) => {
+            return el?.textContent
+        })
+        const payment = await newPage.$eval('#payment', (el) => {
+            return el?.innerText
+        })
+        scrapeData.infomations = {
+            [infomationTitles[0]]: desc,
+            [infomationTitles[1]]: size,
+            [infomationTitles[2]]: delivery,
+            [infomationTitles[3]]: payment,
+        }
+
 
         // console.log(detailLinks)
-        const scraperDetail = async (link) => new Promise(async (resolve, reject) => {
-            try {
-                let pageDetail = await browser.newPage()
-                await pageDetail.goto(link)
-                console.log('>> Truy cập ' + link);
-                await pageDetail.waitForSelector('#main')
+        // const scraperDetail = async (link) => new Promise(async (resolve, reject) => {
+        //     try {
+        //         let pageDetail = await browser.newPage()
+        //         await pageDetail.goto(link)
+        //         console.log('>> Truy cập ' + link);
+        //         await pageDetail.waitForSelector('#main')
 
-                const detailData = {}
-                // bắt đầu cạo
-                // cạo ảnh
-                const images = await pageDetail.$$eval('#left-col > article > div.post-images > div > div.swiper-wrapper > div.swiper-slide', (els) => {
-                    images = els.map(el => {
-                        return el.querySelector('img')?.src
-                    })
-                    return images.filter(i => !i === false)
-                })
-                detailData.images = images
+        //         const detailData = {}
+        //         // bắt đầu cạo
+        //         // cạo ảnh
+        //         const images = await pageDetail.$$eval('#left-col > article > div.post-images > div > div.swiper-wrapper > div.swiper-slide', (els) => {
+        //             images = els.map(el => {
+        //                 return el.querySelector('img')?.src
+        //             })
+        //             return images.filter(i => !i === false)
+        //         })
+        //         detailData.images = images
 
-                // lấy header detail
-                const header = await pageDetail.$eval('header.page-header', (el) => {
-                    return {
-                        title: el.querySelector('h1 > a').innerText,
-                        star: el.querySelector('h1 > span')?.className?.replace(/^\D+/g, ''),
-                        class: {
-                            content: el.querySelector('p').innerText,
-                            classType: el.querySelector('p > a > strong').innerText
-                        },
-                        address: el.querySelector('address').innerText,
-                        attributes: {
-                            price: el.querySelector('div.post-attributes > .price > span').innerText,
-                            acreage: el.querySelector('div.post-attributes > .acreage > span').innerText,
-                            published: el.querySelector('div.post-attributes > .published > span').innerText,
-                            hashtag: el.querySelector('div.post-attributes > .hashtag > span').innerText
-                        }
-                    }
-                })
-                detailData.header = header
-                // thông tin mô tả
-                const mainContentHeader = await pageDetail
-                    .$eval('#left-col > article.the-post > section.post-main-content', (el) => el.querySelector('div.section-header > h2').innerText)
-                const mainContentContent = await pageDetail
-                    .$$eval('#left-col > article.the-post > section.post-main-content > .section-content > p', (els) => els.map(el => el.innerText))
+        //         // lấy header detail
+        //         const header = await pageDetail.$eval('header.page-header', (el) => {
+        //             return {
+        //                 title: el.querySelector('h1 > a').innerText,
+        //                 star: el.querySelector('h1 > span')?.className?.replace(/^\D+/g, ''),
+        //                 class: {
+        //                     content: el.querySelector('p').innerText,
+        //                     classType: el.querySelector('p > a > strong').innerText
+        //                 },
+        //                 address: el.querySelector('address').innerText,
+        //                 attributes: {
+        //                     price: el.querySelector('div.post-attributes > .price > span').innerText,
+        //                     acreage: el.querySelector('div.post-attributes > .acreage > span').innerText,
+        //                     published: el.querySelector('div.post-attributes > .published > span').innerText,
+        //                     hashtag: el.querySelector('div.post-attributes > .hashtag > span').innerText
+        //                 }
+        //             }
+        //         })
+        //         detailData.header = header
+        //         // thông tin mô tả
+        //         const mainContentHeader = await pageDetail
+        //             .$eval('#left-col > article.the-post > section.post-main-content', (el) => el.querySelector('div.section-header > h2').innerText)
+        //         const mainContentContent = await pageDetail
+        //             .$$eval('#left-col > article.the-post > section.post-main-content > .section-content > p', (els) => els.map(el => el.innerText))
 
-                detailData.mainContent = {
-                    header: mainContentHeader,
-                    content: mainContentContent
-                }
-                // đặc ddierm tin đăng
-                const overviewHeader = await pageDetail
-                    .$eval('#left-col > article.the-post > section.post-overview', (el) => el.querySelector('div.section-header > h3').innerText)
-                const overviewContent = await pageDetail
-                    .$$eval('#left-col > article.the-post > section.post-overview > .section-content > table.table > tbody > tr', (els) => els.map(el => ({
-                        name: el.querySelector('td:first-child').innerText,
-                        content: el.querySelector('td:last-child').innerText
-                    })))
+        //         detailData.mainContent = {
+        //             header: mainContentHeader,
+        //             content: mainContentContent
+        //         }
+        //         // đặc ddierm tin đăng
+        //         const overviewHeader = await pageDetail
+        //             .$eval('#left-col > article.the-post > section.post-overview', (el) => el.querySelector('div.section-header > h3').innerText)
+        //         const overviewContent = await pageDetail
+        //             .$$eval('#left-col > article.the-post > section.post-overview > .section-content > table.table > tbody > tr', (els) => els.map(el => ({
+        //                 name: el.querySelector('td:first-child').innerText,
+        //                 content: el.querySelector('td:last-child').innerText
+        //             })))
 
-                detailData.overview = {
-                    header: overviewHeader,
-                    content: overviewContent
-                }
-                // thông tin liên hệ
-                const contactHeader = await pageDetail
-                    .$eval('#left-col > article.the-post > section.post-contact', (el) => el.querySelector('div.section-header > h3').innerText)
-                const contactContent = await pageDetail
-                    .$$eval('#left-col > article.the-post > section.post-contact > .section-content > table.table > tbody > tr', (els) => els.map(el => ({
-                        name: el.querySelector('td:first-child').innerText,
-                        content: el.querySelector('td:last-child').innerText
-                    })))
+        //         detailData.overview = {
+        //             header: overviewHeader,
+        //             content: overviewContent
+        //         }
+        //         // thông tin liên hệ
+        //         const contactHeader = await pageDetail
+        //             .$eval('#left-col > article.the-post > section.post-contact', (el) => el.querySelector('div.section-header > h3').innerText)
+        //         const contactContent = await pageDetail
+        //             .$$eval('#left-col > article.the-post > section.post-contact > .section-content > table.table > tbody > tr', (els) => els.map(el => ({
+        //                 name: el.querySelector('td:first-child').innerText,
+        //                 content: el.querySelector('td:last-child').innerText
+        //             })))
 
-                detailData.contact = {
-                    header: contactHeader,
-                    content: contactContent
-                }
+        //         detailData.contact = {
+        //             header: contactHeader,
+        //             content: contactContent
+        //         }
 
-                await pageDetail.close()
-                console.log('>> Đã đóng tab ' + link)
-                resolve(detailData)
-            } catch (error) {
-                console.log('Lấy data detail lỗi: ' + error);
-                reject(error)
-            }
-        })
-        const details = []
-        for (let link of detailLinks) {
-            const detail = await scraperDetail(link)
-            details.push(detail)
-        }
-        scrapeData.body = details
+        //         await pageDetail.close()
+        //         console.log('>> Đã đóng tab ' + link)
+        //         resolve(detailData)
+        //     } catch (error) {
+        //         console.log('Lấy data detail lỗi: ' + error);
+        //         reject(error)
+        //     }
+        // })
+        // const details = []
+        // for (let link of detailLinks) {
+        //     const detail = await scraperDetail(link)
+        //     details.push(detail)
+        // }
+        // scrapeData.body = details
         console.log('>> Trình duyệt đã đóng.');
         resolve(scrapeData)
     } catch (error) {
@@ -152,5 +237,6 @@ const scraper = (browser, url) => new Promise(async (resolve, reject) => {
 
 module.exports = {
     scrapeCategory,
-    scraper
+    scraper,
+    scrapeItems
 }
